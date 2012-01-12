b8299676ea73b799dff388d31b36571134d84b50
Brandon Pfeiffer <brandonjp@gmail.com>
2012-01-11 21:35:00 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evbryn::Application.config.secret_token = '5015bd28aac7a276c5db5e815ad5f5637b47656a9ffc44b3955bf5a860f1f7d207cdf5ccaeb16479049c351f203e46c41f59ffa1aff0ecd1ab232900138b49b1'
